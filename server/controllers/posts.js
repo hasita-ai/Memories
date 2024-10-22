9aedcaa54d9502a6dbf4782ba964aee74480164f
Hasita <hasita2k@gmail.com>
2024-10-21 21:06:11 -0400

    Removing commented code and adding README

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -27,36 +27,6 @@ export const createPost = async (req, res) => {
   }
 };
 
-
-
-
-
-
-
-// export const getPosts = async (req,res) => {
-//     try {
-//         const postMessages = await PostMessage.find();
-//         res.status(200).json(postMessages);
-//     }
-//     catch (error) {
-//         res.status(404).json({message: error.message});
-//     }
-    
-// }
-
-// export const createPost = async (req, res) => {
-//     const post = req.body;
-//     const newPost = new PostMessage(post);
-//     try {
-//         await newPost.save();
-//         res.status(201).json(newPost);
-//     }
-//     catch (error) {
-//         res.status(409).json({message: error.message});
-//     }
-// }
-
-
 export const updatePost = async (req,res) => {
     const { id: _id } = req.params;
     const post = req.body;
